d864bf025f43e78f24a7eb07d2c9605560e64988
kiryazovkosta <kosta.kiryazov@gmail.com>
2026-01-08 23:11:31 +0200

    Execute as windows service only when OS is Windows

--- XmlSigner/DigitalSignaturesClient/Program.cs
+++ XmlSigner/DigitalSignaturesClient/Program.cs
@@ -3,12 +3,13 @@ using Serilog;
 using DigitalSignaturesClient;
 using DigitalSignaturesClient.Services.Contracts;
 using DigitalSignaturesClient.Services;
+using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("logs/xml-signer.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "logs", "xml-signer-.log"), rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Host.UseSerilog();
@@ -17,10 +18,15 @@ builder.Services.AddScoped<IX509CertificateService, X509CertificateService>();
 builder.Services.AddScoped<ISignVerifyEnvelope, SignVerifyEnvelope>();
 builder.Services.AddScoped<IBase64StringService, Base64StringService>();
 builder.Services.AddHostedService<Worker>();
-builder.Services.AddWindowsService(options =>
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 {
-    options.ServiceName = "Digital Sign Client";
-});
+    builder.Services.AddWindowsService(options =>
+    {
+        options.ServiceName = "Digital Sign Client";
+    });
+}
+
 builder.Services.AddControllers().AddXmlDataContractSerializerFormatters();
 builder.Services.AddCors(options =>
 {
